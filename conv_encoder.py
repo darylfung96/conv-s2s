1d56b1f2b70b0316a0d7280a8ec3e53e58d2eea2
Daryl Fung <darylfung96@gmail.com>
2018-03-25 22:20:16 -0500

    added embedding

--- conv_encoder.py
+++ conv_encoder.py
@@ -1,11 +1,26 @@
 import torch
 import torch.nn as nn
-
+import torch.functional as F
 
 class ConvEncoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, vocab_size, hidden_size, kernel_size, dropout):
         super(ConvEncoder, self).__init__()
 
+        self._embedding_size = 512
+        self._vocab_size = vocab_size
+        self._hidden_size = hidden_size
+        self._dropout = dropout
+
+        self.embedding = nn.Embedding(vocab_size, self._embedding_size)
+
+        #TODO create embedding position
+        # self.embedding_pos = nn.Embedding()
+        self.fc1 = nn.Linear(self._embedding_size, self._hidden_size)
+        self.kernel_size = kernel_size
+        self.conv = nn.Conv1d(hidden_size, hidden_size, kernel_size)
+
     def forward(self, input):
+
+        embedded_input = self.embedding(input)
         pass
