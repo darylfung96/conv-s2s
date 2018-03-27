8ba58695d3a7b4570ad6810b767c7722b1bb2c37
Daryl Fung <darylfung96@gmail.com>
2018-03-26 21:28:59 -0500

    added encoder layer

--- conv_encoder.py
+++ conv_encoder.py
@@ -1,26 +1,56 @@
-import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
+
+import math
+
 
 class ConvEncoder(nn.Module):
 
-    def __init__(self, vocab_size, hidden_size, kernel_size, dropout):
+    def __init__(self, vocab_size, hidden_size, kernel_size, num_layers, dropout, training):
         super(ConvEncoder, self).__init__()
 
         self._embedding_size = 512
         self._vocab_size = vocab_size
         self._hidden_size = hidden_size
+        self._conv_out_channels = 2 * self._hidden_size
+        self._num_layers = num_layers
         self._dropout = dropout
+        self.is_training = training
 
         self.embedding = nn.Embedding(vocab_size, self._embedding_size)
 
         #TODO create embedding position
         # self.embedding_pos = nn.Embedding()
         self.fc1 = nn.Linear(self._embedding_size, self._hidden_size)
+        self.fc2 = nn.Linear(self._hidden_size, self._embedding_size)
         self.kernel_size = kernel_size
-        self.conv = nn.Conv1d(hidden_size, hidden_size, kernel_size)
+        self.conv = nn.Conv1d(hidden_size, self._conv_out_channels, kernel_size)
 
     def forward(self, input):
 
-        embedded_input = self.embedding(input)
-        pass
+        # embedding
+        embedded_input = self.embedding(input) # + self.embedding_position(input)
+        embedded_input = F.dropout(embedded_input, p=self._dropout, training=self.is_training)
+
+        fc1_output = self.fc1(embedded_input)
+
+        layer_output = embedded_input
+
+        for _ in range(self._num_layers):
+            residual_output = layer_output
+
+            fc1_output = F.dropout(fc1_output, p=self._dropout)
+
+            conv_output = self.conv(embedded_input)
+            glu_output = F.glu(conv_output, 2)
+
+            layer_output = (glu_output + residual_output) * math.sqrt(0.5) # scale value
+
+
+        # convert back to embedding dimension
+        encoder_output = self.fc2(layer_output)
+        encoder_attention = (encoder_output + embedded_input) * math.sqrt(0.5)
+
+        return encoder_output, encoder_attention
+
+
