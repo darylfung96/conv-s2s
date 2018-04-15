8d139a52e5cffd6271a5bc1deb73371a3f9f0e6c
Daryl Fung <darylfung96@gmail.com>
2018-04-15 18:08:48 -0500

    finished eval function

--- seq2seq.py
+++ seq2seq.py
@@ -9,16 +9,32 @@ class Seq2seq(nn.Module):
         self._encoder = encoder
         self._decoder = decoder
 
-    def forward(self, input, target):
+    def forward(self, input, target=None, is_training=True):
+        if is_training:
+            if target is None:
+                raise ValueError("target parameter has to be passed with value.")
+            return self.start_train(input, target)
+        else:
+            return self.start_eval(input)
+
+    def start_train(self, input, target):
         encoder_output, encoder_attention = self._encoder(input)
 
-        # decoder_whole_output = Variable(torch.from_numpy(numpy.array([[0, 0, 0, 1]]*input.shape[0])))
-        decoder_whole_output = Variable(torch.from_numpy(numpy.array(target)))
-        iteration = 0
-        while any(decoder_whole_output.data[:][-1]) != 2 and iteration < 15:
-            decoder_output = self._decoder(decoder_whole_output, encoder_output, encoder_attention)
-            next_decoder_output = torch.max(decoder_output, 2)[1] # get the highest probability for the next word
-            decoder_whole_output = torch.cat((decoder_whole_output, next_decoder_output), 1)
-            iteration += 1
+        decoder_inputs = Variable(torch.from_numpy(numpy.array(target)))
+        decoder_output = self._decoder(decoder_inputs, encoder_output, encoder_attention)
 
-        return decoder_whole_output
+        #TODO do loss criterion with target and predicted output
+        pass
+
+    def start_eval(self, input):
+        encoder_output, encoder_attention = self._encoder(input)
+
+        decoder_inputs = Variable(torch.from_numpy(numpy.array([[1]] * encoder_output.size(0))))
+        next_decoder_output = None
+
+        while next_decoder_output is None or next_decoder_output.data[0][0] is not 2:
+            decoder_output = self._decoder(decoder_inputs, encoder_output, encoder_attention)
+            next_decoder_output = torch.max(decoder_output, 3)[1][:, :, -1]
+            decoder_inputs = torch.cat([decoder_inputs, next_decoder_output], dim=1)
+
+        return decoder_inputs
