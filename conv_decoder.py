fix log softmax

conv_decoder.py
@@ -19,7 +19,7 @@ class ConvDecoder(nn.Module):
         self._is_training = is_training
 
         self.embedding = nn.Embedding(vocab_size+3, self._embedding_size)
-        self.embedding_position = EmbeddingPosition(self._vocab_size+3, self._embedding_size) # + 1 to include padding which act as none
+        self.embedding_position = EmbeddingPosition(max_length, self._embedding_size) # + 1 to include padding which act as none
 
         self.fc1 = nn.Linear(embedding_size, hidden_size)
         self.conv = nn.Conv2d(1, 2 * hidden_size, self._kernel_size, padding=(self._kernel_size[0]-1, 0))
@@ -61,6 +61,6 @@ class ConvDecoder(nn.Module):
         fc2_output = self.fc2(layer_output)
         fc2_output = F.dropout(fc2_output, p=self._dropout, training=self._is_training)
         fc3_output = self.fc3(fc2_output)
-        prob_output = F.log_softmax(fc3_output, 2)
+        prob_output = F.log_softmax(fc3_output, 3)
 
         return prob_output

seq2seq.py
@@ -48,7 +48,7 @@ class Seq2seq(nn.Module):
         decoder_inputs = Variable(torch.from_numpy(numpy.array([[1]] * encoder_output.size(0))))
         next_decoder_output = None
 
-        while next_decoder_output is None or next_decoder_output.data[0][0] is not 1:
+        while (next_decoder_output is None or next_decoder_output.data[0][0] is not 1) and len(decoder_inputs.data[0]) < self._decoder._max_length:
             decoder_output = self._decoder(decoder_inputs, encoder_output, encoder_attention)
             next_decoder_output = torch.max(decoder_output, 3)[1][:, :, -1]
             decoder_inputs = torch.cat([decoder_inputs, next_decoder_output], dim=1)

train.py
@@ -78,8 +78,8 @@ for index in range(len(examples_target)):
 examples = [np.pad(example, [0, max_input_length+2-len(example)], mode='constant') for example in examples] # + 1 to add the end token
 examples_target =[np.pad(example, [0, max_target_length+2-len(example)], mode='constant') for example in examples_target] # + 1 to add the end token
 
-conv_encoder = ConvEncoder(len(word_to_index), max_input_length, hidden_size=128, embedding_size=512, num_layers=1, dropout=0, is_training=True)
-conv_decoder = ConvDecoder(len(word_to_index), max_target_length, hidden_size=128, embedding_size=512, num_layers=1, dropout=0, is_training=True)
+conv_encoder = ConvEncoder(len(word_to_index), max_input_length+2, hidden_size=128, embedding_size=512, num_layers=1, dropout=0, is_training=True)
+conv_decoder = ConvDecoder(len(word_to_index), max_target_length+2, hidden_size=128, embedding_size=512, num_layers=1, dropout=0, is_training=True)
 
 examples = np.array(examples)
 examples_target = np.array(examples_target)
@@ -91,7 +91,7 @@ seq_output = seq_output.data.numpy()
 sentences = [index_to_word_sentence(seq) for seq in seq_output]
 print(sentences)
 
-#TODO fix evaluation for seq2seq
+#TODO fix evaluation keep showing end end
 new_text = input('type in text to predict:')
 new_text_token = np.array([[word_to_index[token] for token in new_text.lower().split()]])
 outputs = seq2seq(new_text_token, is_training=False)